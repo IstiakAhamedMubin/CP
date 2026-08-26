140c4cc825c59c5882571da28995eb0fa95153e0
Istiak Ahamed <istiakop800@gmail.com>
2026-08-26 20:11:45 +0600

    Add Domino Piling solution

---
+++ Codeforces/Domino_Piling.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+   
+    int M,N;
+    cin>> M >> N;
+
+    int max_dominos = (M * N)/2;
+
+    cout<< max_dominos << endl;
+    return 0;
+
+}
